dbef02bdb647eca2e2e9969d0ba6cff204ef1520
cfehring <hirechrisf@gmail.com>
2023-01-23 21:00:35 -0800

    Try routing to document instead of path
    There's no index in tutorial-basics or tutorial-extras so the links need to go to specific files.

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -6,7 +6,7 @@ import Link from '@docusaurus/Link';
 const FeatureList = [
   {
     title: 'Learn to code',
-    url: '@site/docs/intro',
+    url: '/docs/intro',
     Svg: require('@site/static/img/coding-monitor-svgrepo-com.svg').default,
     description: (
       <>
@@ -16,7 +16,7 @@ const FeatureList = [
   },
   {
     title: 'FRC and WPILib',
-    url: '@site/docs/tutorial-basics',
+    url: '/docs/tutorial-basics/congratulations',
     Svg: require('@site/static/img/wpilibdocslogo.svg').default,
     description: (
       <>
@@ -26,7 +26,7 @@ const FeatureList = [
   },
   {
     title: 'Advanced Topics',
-    url: '@site/docs/tutorial-extras',
+    url: '/docs/tutorial-extras/manage-docs-versions',
     Svg: require('@site/static/img/penguin_admin.svg').default,
     description: (
       <>
